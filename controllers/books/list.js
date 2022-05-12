3b48bec2c140dc9426e009ce232795657f1f57df
lfabl <met2net3@gmail.com>
2022-05-12 20:00:41 +0300

    Feature: Filter feature added to book list page.

--- controllers/books/list.js
+++ controllers/books/list.js
@@ -1,13 +1,32 @@
 const Book = require('../../models/Book');
+const Author = require('../../models/Author');
 
 module.exports = function (req, res, next) {
+  const $or = [];
+
+  if (req.query.q) {
+    const searchText = decodeURI(req.query.q);
+    $or.push({ title: searchText });
+  }
+
+  if (req.query.author) {
+    const authorId = decodeURI(req.query.author);
+    $or.push({ author: authorId });
+  }
+
   Book
-    .find({}, 'title author')
+    .find($or.length ? { $or } : {}, 'title author')
     .sort({ title: 1 })
     .populate('author')
     .exec(function (err, listBooks) {
       if (err) return next(err);
 
-      res.render('book_list', { title: 'Book List', book_list: listBooks });
+      Author
+        .find({}, 'first_name family_name id')
+        .exec(function (err, listAuthors) {
+          if (err) return next(err);
+
+          res.render('book_list', { title: 'Book List', book_list: listBooks, author_list: listAuthors });
+        });
     });
 };
